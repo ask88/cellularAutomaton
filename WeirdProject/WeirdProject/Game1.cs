7e279911d173513ed7f8d6d42d798d57ed9a9ca9
Aaron Kiser <aaronkiser88@gmail.com>
2018-10-22 16:08:12 -0400

    intitial commit

---
+++ WeirdProject/WeirdProject/Game1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace WeirdProject
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        private const int WIDTH = 200;
+        private const int HEIGHT = 200;
+
+        private const int TILEWIDTH = 20;
+        private const int TILEHEIGHT = 20;
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        MapClass map;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            map = new MapClass(this, TILEWIDTH, TILEHEIGHT);
+            graphics.PreferredBackBufferWidth = WIDTH;
+            graphics.PreferredBackBufferHeight = HEIGHT;
+            graphics.ApplyChanges();
+            IsMouseVisible = true;
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            map.Load();
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+            map.Unload();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            map.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            map.Draw(spriteBatch);
+            base.Draw(gameTime);
+        }
+
+        public int getWidth() { return WIDTH; }
+        public int getHeight() { return HEIGHT; }
+    }
+}
---
+++ WeirdProject/WeirdProject/MapClass.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace WeirdProject
+{
+    public class Block
+    {
+        private int tw, th;
+        private Game1 g;
+        private Texture2D filledBlock;
+        private Color blockColor;
+
+        public Block(Game1 g, Color blockColor, int tw, int th)
+        {
+            this.g = g;
+            this.blockColor = blockColor;
+            this.tw = tw;
+            this.th = th;
+            block();
+        }
+
+        public void setColor(Color blockColor) { this.blockColor = blockColor; }
+        public int getTileWidth() { return tw; }
+        public int getTileHeight() { return th; }
+        public Texture2D getBlock() { return filledBlock; }
+
+        private void block()
+        {
+            Texture2D b = new Texture2D(g.GraphicsDevice, tw, th);
+            Color[] colorData = new Color[tw * th];
+            for(int i = 0; i < colorData.Length; i++)
+            {
+                colorData[i] = blockColor;
+            }
+            b.SetData<Color>(colorData);
+            filledBlock = b;
+        }
+
+    }
+
+    public class MapClass
+    {
+        public Game1 g;
+        private int ScreenWidth;
+        private int ScreenHeight;
+        private int MapWidth;
+        private int MapHeight;
+        private int TileWidth;
+        private int TileHeight;
+        private Block[] block;
+
+      public MapClass(Game1 g, int TileWidth, int TileHeight)
+        {
+            ScreenWidth = g.getWidth();
+            ScreenHeight = g.getHeight();
+            MapWidth = ScreenWidth / TileWidth;
+            MapHeight = ScreenHeight / TileHeight;
+
+            block = new Block[MapWidth * MapHeight];
+            for(int y = 0; y < MapHeight; y++)
+            {
+                for(int x = 0; x < MapWidth; x++)
+                {
+                    block[x + y * MapWidth] = new Block(g, Green(), TileWidth, TileHeight);
+                }
+            }
+
+            this.TileWidth = TileWidth;
+            this.TileHeight = TileHeight;
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public void Unload()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            for(int y = 0; y < MapHeight; y++)
+            {
+                for(int x = 0; x < MapWidth; x++)
+                {
+                    spriteBatch.Draw(block[x + y * MapWidth].getBlock(), new Vector2(x * TileWidth, y * TileHeight), Color.White);
+                }
+            }           
+            spriteBatch.End();
+        }
+
+        private void DrawBlock()
+        {
+            MouseState state = Mouse.GetState();
+            /*
+            if(state.LeftButton == ButtonState.Pressed &&
+                state.Position.X < )
+            {
+
+            }
+            */
+        }
+
+        private Color Black()
+        {
+            int r = 0;
+            int g = 0;
+            int b = 0;
+            int a = 255;
+
+            return new Color(r, g, b, a);
+        }
+        private Color Red()
+        {
+            int r = 255;
+            int g = 0;
+            int b = 0;
+            int a = 255;
+
+            return new Color(r, g, b, a);
+        }
+        private Color Green()
+        {
+            int r = 0;
+            int g = 255;
+            int b = 0;
+            int a = 255;
+
+            return new Color(r, g, b, a);
+        }
+        private Color Blue()
+        {
+            int r = 0;
+            int g = 0;
+            int b = 255;
+            int a = 255;
+
+            return new Color(r, g, b, a);
+        }
+    }
+}
